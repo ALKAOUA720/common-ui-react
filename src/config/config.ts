170ebee54c9f1e0c9ebdf96500fc037eb1c58f6b
ALKAOUA720 <929350256@qq.com>
2023-07-26 11:09:41 +0800

    🎉 init(全部): 提交到远程仓库

---
+++ src/config/config.ts
@@ -0,0 +1,27 @@
+export interface listType {
+  component: string;
+  name: string;
+}[]
+
+export const componentsList: listType[] = [
+  {
+    component: 'Popover',
+    name: '气泡卡片',
+  },
+  {
+    component: 'Drawer',
+    name: '抽屉',
+  },
+  {
+    component: 'Modal',
+    name: '弹窗',
+  },
+  {
+    component: 'Toast',
+    name: '轻提示',
+  },
+  {
+    component: 'Message',
+    name: '全局提示',
+  },
+]
